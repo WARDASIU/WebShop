c5f254b860d2a095188ed651d78a7d2d7a220bd8
WARDASIU <pepe-pepe2044@wp.pl>
2022-12-29 18:29:15 +0100

    Home page without Product handling ready.

--- src/main/java/com/wardasiu/project/wardasiu/controllers/ProductsController.java
+++ src/main/java/com/wardasiu/project/wardasiu/controllers/ProductsController.java
@@ -1,5 +1,6 @@
 package com.wardasiu.project.wardasiu.controllers;
 
+import com.google.gson.Gson;
 import com.wardasiu.project.wardasiu.entities.Product;
 import com.wardasiu.project.wardasiu.repositories.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,26 +23,24 @@ public class ProductsController {
         this.productsRepository = productsRepository;
     }
 
-
-    @GetMapping
-    @RequestMapping("/api/products")
-    public List<Product> findAllProducts() {
-        return productsRepository.findAll();
-    }
-
-    @GetMapping()
-    @RequestMapping("/product/{id}")
+    @GetMapping("/product/{id}")
     public ResponseEntity<Product> findProductsById(@PathVariable(value = "id") long id) {
         Optional<Product> product = productsRepository.findProductByIdProducts(id);
 
         return product.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/products")
-    public ModelAndView getProductsAsList() {
-        ModelAndView mav = new ModelAndView("products");
-        mav.addObject("products", productsRepository.findAll());
+    @GetMapping("/api/products/images/{filename}")
+    @ResponseBody
+    public byte[] getImage(@PathVariable String filename) throws IOException {
+        String filePath = Paths.get("src/main/resources/img", filename).toString();
+        return Files.readAllBytes(Paths.get(filePath));
+    }
 
-        return mav;
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public String getProductsAsList() {
+        Gson gson = new Gson();
+
+        return gson.toJson(productsRepository.findAll());
     }
 }
